90686b7aefade8361cd2177bb3b47a975b1861ce
”Frederik <fred295q@stud.kea.dk>
2023-02-23 12:26:04 +0100

    forside done

--- productsite.js
+++ productsite.js
@@ -35,8 +35,6 @@ function showProducts(data) {
     copy.querySelector(".alkoholfri").textContent = "";
   }
 
-  //   copy.querySelector(".ui-card").onClick = "location.href='product-page.html?q=name:'+ data.name";
-
   copy.querySelector(".ui-card").addEventListener("click", function () {
     location.href = "product-page.html?q:name=" + data.name;
   });
